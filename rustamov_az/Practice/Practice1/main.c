e7f17ed2c97c235512cb868aedc8bb13fd3d8f17
Rustamov_AZ <azer.rustamov.01@gmail.com>
2018-11-04 19:48:13 +0300

    done

--- rustamov_az/Practice/Practice1/main.c
+++ rustamov_az/Practice/Practice1/main.c
@@ -13,6 +13,7 @@ void main()
 		printf("Ошибка: Высота шкафа выходит за заданные пределы\n");
 		return;
 	}
+	h /= 100.0;
 	printf("Укажите ширину шкафа в см(от 80 до 120 см)\n");
 	scanf("%d", &w);
 	if ((w > 120) || (w < 80))
@@ -20,6 +21,7 @@ void main()
 		printf("Ошибка: Ширина шкафа выходит за заданные пределы\n");
 		return;
 	}
+	w /= 100.0;
 	printf("Укажите глубину шкафа в см(от 50 до 90 см)\n");
 	scanf("%d", &d);
 	if ((d > 90) || (d < 50))
@@ -27,9 +29,11 @@ void main()
 		printf("Ошибка: Глубина шкафа выходит за заданные пределы\n");
 		return;
 	}
+	d /= 100.0;
 	printf("Введите плотность ДВП в кг/м^3 \n");
 	scanf("%lf", &pDVP);
-	if (pDVP <= 0) {
+	if (pDVP <= 0) 
+	{
 		printf("Ошибка: Плотность должна быть больше нуля\n");
 		return;
 	}
@@ -43,15 +47,16 @@ void main()
 
 	printf("Введите плотность дерева в кг/м^3 \n");
 	scanf("%lf", &pWOOD);
-	if (pWOOD <= 0) {
+	if (pWOOD <= 0) 
+	{
 		printf("Ошибка: Плотность должна быть больше нуля\n");
 		return;
 	}
-	mBack = (h * w * 0.5 * pDVP);
-	mSides = (2 * h * d* 1.5 * pDSP);
-	mTop = (2 * w * d * 1.5 * pDSP);
-	mDoors = (h * w * 1 * pWOOD);
-	mShelf = d * (w - 3) * 1.5 * floor(h / 40);
-	Mass = (mBack + mSides + mTop + mDoors + mShelf) / 1000000;
+	mBack = (h * w * 0.005 * pDVP);
+	mSides = (2 * h * d * 0.015 * pDSP);
+	mTop = (2 * w * d * 0.015 * pDSP);
+	mDoors = (h * w * 0.01 * pWOOD);
+	mShelf = (h - 2 * 0.015) / (0.4 + 0.015) * (w - 2 * 0.015) * (d - 0.005) * 0.015 * pDSP;
+	Mass = mBack + mSides + mTop + mDoors + mShelf;
 	printf("Масса шкафа: %lf кг \n", Mass);
 }	
