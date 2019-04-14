33d0b2ba1f7c3bf8faa0718d5462afdbbe99706b
RustamovAzer <azer.rustamov.01@gmail.com>
2019-04-14 20:08:14 +0300

    14.04

---
+++ rustamov_az/Practice/Practice8/Matrix.cpp
@@ -0,0 +1,157 @@
+#include "Matrix.h"
+
+Matrix::Matrix()
+{
+    rows = 0;
+    cols = 0;
+    m = nullptr;
+}
+
+Matrix::Matrix(const Matrix& _m)
+{
+    rows = _m.rows;
+    cols = _m.cols;
+    unsigned elements = rows * cols;
+    m = new double[elements];
+    for (unsigned i = 0; i < elements; i++)
+        m[i] = _m.m[i];
+}
+
+Matrix::Matrix(unsigned _rows, unsigned _cols)
+{
+    rows = _rows;
+    cols = _cols;
+    unsigned elements = rows * cols;
+    m = new double[elements];
+    for (unsigned i = 0; i < elements; i++)
+        m[i] = 0;
+}
+
+Matrix::Matrix(double* _m, unsigned _rows, unsigned _cols)
+{
+    rows = _rows;
+    cols = _cols;
+    unsigned elements = rows * cols;
+    m = new double[elements];
+    for (unsigned i = 0; i < elements; i++)
+        m[i] = _m[i];
+}
+
+Matrix::~Matrix()
+{
+    rows = 0;
+    cols = 0;
+    delete[] m;
+}
+
+void Matrix::output()
+{
+    for (unsigned i = 0; i < rows; i++)
+    {
+        for (unsigned j = 0; j < cols; j++)
+            std::cout << m[i * cols + j] << " ";
+        std::cout << '\n';
+    }
+}
+
+void Matrix::input()
+{
+    std::cout << "Введите число строк: ";
+    std::cin >> rows;
+    std::cout << "Введите число столбцов: ";
+    std::cin >> cols;
+    *this = Matrix(rows, cols);
+    std::cout << "Введите значения элементов матрицы: ";
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+    {
+        std::cin >> m[i];
+    }
+}
+
+Matrix Matrix::operator+(const Matrix& _m)
+{
+    if ((rows != _m.rows) || (cols != _m.cols))
+        throw "Размеры матриц не совпадают\n";
+    Matrix result(rows, cols);
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+        result.m[i] = m[i] + _m.m[i];
+    return result;
+}
+
+Matrix Matrix::operator+(double d)
+{
+    if ((rows == 0) || (cols == 0))
+        return Matrix();
+    Matrix result(rows, cols);
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+        result.m[i] = m[i] + d;
+    return result;
+}
+
+Matrix Matrix::operator-(const Matrix& _m)
+{
+    if ((rows != _m.rows) || (cols != _m.cols))
+        throw "Размеры матриц не совпадают\n";
+    Matrix result(rows, cols);
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+        result.m[i] = m[i] - _m.m[i];
+    return result;
+}
+
+Matrix Matrix::operator-(double d)
+{
+    if (!rows || !cols)
+        return Matrix();
+    Matrix result(rows, cols);
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+        result.m[i] = m[i] - d;
+    return result;
+}
+
+Matrix Matrix::operator*(const Matrix& _m)
+{
+    if ((rows != _m.cols) || (cols != _m.rows))
+        throw "Невозможно провести умножение\n";
+    Matrix result(rows, _m.cols);
+    for (unsigned i = 0; i < rows; i++)
+        for (unsigned j = 0; j < cols; j++)
+            for (unsigned k = 0; k < result.cols; k++)
+                result.m[i * cols + j] += m[i * result.cols + k] * _m.m[k * _m.cols + j];
+    return result;
+}
+
+Matrix Matrix::operator*(double d)
+{
+    if ((rows == 0) || (cols == 0))
+        return Matrix();
+    Matrix result(rows, cols);
+    unsigned elements = rows * cols;
+    for (unsigned i = 0; i < elements; i++)
+        result.m[i] = m[i] * d;
+    return result;
+}
+
+const Matrix& Matrix::operator=(const Matrix& _m)
+{
+    if ((rows == _m.rows) && (cols == _m.cols) && (m == _m.m))
+        return *this;
+    if (rows ^ _m.rows ^ cols ^ _m.cols) // сравним все хором
+        delete[] m;
+    rows = _m.rows;
+    cols = _m.cols;
+    unsigned elements = rows * cols;
+    m = new double[elements];
+    for (unsigned i = 0; i < elements; i++)
+        m[i] = _m.m[i];
+    return *this;
+}
+
+double* Matrix::operator[](unsigned index)
+{
+    return m + index * cols;
+}
