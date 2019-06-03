009be9103430af59c42b4e1724371dea1774b809
RustamovAzer <azer.rustamov.01@gmail.com>
2019-06-03 10:55:26 +0300

    03.06

--- rustamov_az/Practice/Practice7/Nvector.h
+++ rustamov_az/Practice/Practice7/Nvector.h
@@ -23,7 +23,7 @@ public:
     double operator*(const Nvector & _x);
     Nvector operator*(const double m);
     double & operator[](int index);
-    double Length();
+    double Lenght();
     Nvector operator+=(const Nvector & temp);
     Nvector operator+=(const double d);
     Nvector operator-=(const Nvector & _x);
@@ -38,5 +38,32 @@ public:
     void Input();
     friend istream & operator>> (istream & in, Nvector & _vector);
     friend ostream & operator<<(ostream & out, const Nvector & _vector);
+};
+
+class VectorIncorrectVariablesException : std::exception
+{
+    const std::string what_str = "Incorrect variables";
+public:
+    const char* what() const;
+};
+
+class VectorUnequalDimensionException : std::exception
+{
+    const std::string what_str = "Unequal dimension";
+public:
+    const char* what() const;
+};
+
+class VectorOutOfBoundsException : std::exception
+{
+    const std::string what_str = "Out of bounds";
+public:
+    const char* what() const;
+};
+class VectorNotEnoughMemoryException : std::exception
+{
+    const std::string what_str = "Not enough memory";
+public:
+    const char* what() const;
 };
 
